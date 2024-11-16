0e2c8bcece26b2d88d465cba7393e6ba4507e250
James Xu <jamesxu1288@gmail.com>
2024-11-16 00:37:34 -0500

    [REMOVE ME] Revert FlashInfer version
    https://github.com/flashinfer-ai/flashinfer/pull/609 potentially introduces correctness issues

--- scripts/ci_install_dependency.sh
+++ scripts/ci_install_dependency.sh
@@ -6,6 +6,7 @@ pip install --upgrade pip
 pip install -e "python[all]"
 pip install transformers==4.45.2 sentence_transformers accelerate peft
 git clone https://github.com/flashinfer-ai/flashinfer.git --recursive
+git reset --hard 32d9510d67187f1f3a379cce81302cdd15a557d2 # Revert to before PR https://github.com/flashinfer-ai/flashinfer/pull/609 merged
 cd flashinfer/python
 pip install -e .
 pip install ninja # Ninja is required to load C++ extensions
